f9adfd134e7c00918a902ed40dc350949dac10b7
M. Hamza Noor <75859619+HamzaRandhawa@users.noreply.github.com>
2025-03-03 13:37:50 -0500

    Update video-modal.js

--- assets/js/video-modal.js
+++ assets/js/video-modal.js
@@ -10,9 +10,17 @@
 function openVideoModal(videoUrl) {
   var modal = document.getElementById("videoModal");
   var iframe = document.getElementById("videoFrame");
+
+  // Convert Google Drive share link to embeddable format
+  if (videoUrl.includes("drive.google.com")) {
+    let fileIdMatch = videoUrl.match(/[-\w]{25,}/); // Extract file ID
+    if (fileIdMatch) {
+      videoUrl = "https://drive.google.com/file/d/" + fileIdMatch[0] + "/preview";
+    }
+  }
   
   // Set video source (allow YouTube, Vimeo, or uploaded files)
-  iframe.src = videoUrl.includes("http") ? videoUrl : "/assets/videos/" + videoUrl;
+  // iframe.src = videoUrl.includes("http") ? videoUrl : "/assets/videos/" + videoUrl;
   
   modal.style.display = "flex"; // Show modal
 }
